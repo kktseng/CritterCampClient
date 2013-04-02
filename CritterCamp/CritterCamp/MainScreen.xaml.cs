2d26243c1d5c1b6d1bf13b00b29b1463e890aaeb
Alex Leung <leungxa@gmail.com>
2013-04-02 01:15:41 -0700

    new Login screen

--- CritterCamp/CritterCamp/MainScreen.xaml.cs
+++ CritterCamp/CritterCamp/MainScreen.xaml.cs
@@ -31,36 +31,36 @@ namespace CritterCamp {
         string password;
 
         void onLoaded(object sender, RoutedEventArgs e) {
-            Status.Text = ""; // clear any messages 
+            Status.Text = ""; // clear any messages
 
             // check and see if we have a tcp connection already
-            if (CoreApplication.Properties.ContainsKey("TCPSocket")) {
+            if(CoreApplication.Properties.ContainsKey("TCPSocket")) {
                 ((TCPConnection)CoreApplication.Properties["TCPSocket"]).Disconnect(); // disconnect the exisiting connection
                 CoreApplication.Properties.Remove("TCPSocket");
             }
 
             // check and see if we have an error message to display
-            if (CoreApplication.Properties.ContainsKey("error")) {
+            if(CoreApplication.Properties.ContainsKey("error")) {
                 Status.Text = (string)CoreApplication.Properties["error"]; // show the error message
                 CoreApplication.Properties.Remove("error");
 
                 UserInput.Visibility = Visibility.Collapsed; // hide the input boxes
                 PlayButton.Visibility = Visibility.Collapsed; // hide the play button
                 ResumeButton.Visibility = Visibility.Visible; // show the resume button
-            } else 
-            // get previous login information if it exists
-            if (IsolatedStorageSettings.ApplicationSettings.TryGetValue<String>("username", out username) &&
-                IsolatedStorageSettings.ApplicationSettings.TryGetValue<String>("password", out password)) {
-                // login information is already in the app
+            } else
+                // get previous login information if it exists
+                if(IsolatedStorageSettings.ApplicationSettings.TryGetValue<String>("username", out username) &&
+                    IsolatedStorageSettings.ApplicationSettings.TryGetValue<String>("password", out password)) {
+                    // login information is already in the app
 
-                UserInput.Visibility = Visibility.Collapsed; // hide the input boxes
-                PlayButton.Visibility = Visibility.Visible; // show the play button
-                Status.Text += "Welcome back " + username + "!";
-            } else {
-                // no previous login information. ask the user for their information
-                UserInput.Visibility = Visibility.Visible; // show the input boxes
-                PlayButton.Visibility = Visibility.Collapsed; // hide the play button
-            }
+                    UserInput.Visibility = Visibility.Collapsed; // hide the input 
+                    PlayButton.Visibility = Visibility.Visible; // show the play button
+                    Status.Text += "Welcome back " + username + "!";
+                } else {
+                    // no previous login information. ask the user for their information
+                    UserInput.Visibility = Visibility.Visible; // show the input boxes
+                    PlayButton.Visibility = Visibility.Collapsed; // hide the play button
+                }
         }
 
         private void Register_Click(object sender, RoutedEventArgs e) {
@@ -82,45 +82,47 @@ namespace CritterCamp {
         }
 
         private void Play_Click(object sender, RoutedEventArgs e) {
+            // hide buttons when pressed
+            if(PlayButton.Visibility == Visibility.Visible)
+                PlayButton.Visibility = Visibility.Collapsed;
+            if(ResumeButton.Visibility == Visibility.Visible)
+                ResumeButton.Visibility = Visibility.Collapsed;
+
             Login(false);
         }
 
         private bool ValidateUsername(string u) {
-            if (u.Length <= 0) {
+            if(u.Length <= 0) {
                 Status.Text = "Please enter a username.";
                 return false;
             }
-
-            if (u.Length > 15) {
+            if(u.Length > 15) {
                 Status.Text = "Username must be 15 characters or less.";
                 return false;
             }
-
-            foreach (char c in u) {
-                if (!char.IsLetterOrDigit(c)) {
+            foreach(char c in u) {
+                if(!char.IsLetterOrDigit(c)) {
                     Status.Text = "Username can only contain letters or numbers.";
                     return false;
                 }
             }
-
             return true;
         }
 
         private bool ValidatePassword(string p) {
-            if (p.Length <= 0) {
+            if(p.Length <= 0) {
                 Status.Text = "Please enter a password.";
                 return false;
             }
-
             return true;
         }
 
         private async void Register() {
-            if (connecting) // only allow one connection at once
+            if(connecting) // only allow one connection at once
                 return;
             connecting = true;
 
-            if (!ValidateUsername(username)) { // only allow valid usernames to login
+            if(!ValidateUsername(username)) { // only allow valid usernames to login
                 connecting = false;
                 return;
             }
@@ -129,16 +131,15 @@ namespace CritterCamp {
             string postData = string.Format(postDataFormat, username, password);
 
             HTTPConnectionResult registerResult = await HTTPConnection.GetPostResult(urlRegister, postData);
-            if (!registerResult.error) { // not an error connecting to server
+            if(!registerResult.error) { // not an error connecting to server
                 LoginResponse response = new LoginResponse(registerResult.message);
                 Status.Text = response.message;
 
                 connecting = false;
-                if (response.success) { // registering was sucessful
+                if(response.success) { // registering was sucessful
                     // Log users in automatically
                     Login(true);
-                } 
-
+                }
             } else {
                 // there was an error when connecting to the http server
                 System.Diagnostics.Debug.WriteLine("Error connecting to http server");
@@ -148,34 +149,34 @@ namespace CritterCamp {
         }
 
         private async void Login(bool fromRegister) {
-            if (connecting) // only allow one connection at once
+            if(connecting) // only allow one connection at once
                 return;
             connecting = true;
 
-            if (!ValidateUsername(username)) { // only allow valid usernames to login
+            if(!ValidateUsername(username)) { // only allow valid usernames to login
                 connecting = false;
                 return;
             }
 
-            if (fromRegister) // this login call was from a register
+            if(fromRegister) // this login call was from a register
                 Status.Text += " Logging in...";
             else
                 Status.Text = "Logging in...";
-            string postData = string.Format(postDataFormat, username, password);            
+            string postData = string.Format(postDataFormat, username, password);
 
             HTTPConnectionResult loginResult = await HTTPConnection.GetPostResult(urlLogin, postData);
-            if (!loginResult.error) { // not an error connecting to server
+            if(!loginResult.error) { // not an error connecting to server
                 LoginResponse response = new LoginResponse(loginResult.message);
                 Status.Text = response.message;
 
-                if (response.success) { // login was sucessful
+                if(response.success) { // login was sucessful
                     // save the username and password in local settings so that it can load it later
-                    if (IsolatedStorageSettings.ApplicationSettings.Contains("username")) {
+                    if(IsolatedStorageSettings.ApplicationSettings.Contains("username")) {
                         IsolatedStorageSettings.ApplicationSettings["username"] = username;
                     } else {
                         IsolatedStorageSettings.ApplicationSettings.Add("username", username);
                     }
-                    if (IsolatedStorageSettings.ApplicationSettings.Contains("password")) {
+                    if(IsolatedStorageSettings.ApplicationSettings.Contains("password")) {
                         IsolatedStorageSettings.ApplicationSettings["password"] = password;
                     } else {
                         IsolatedStorageSettings.ApplicationSettings.Add("password", password);
@@ -204,12 +205,12 @@ namespace CritterCamp {
                          * Party updates
                          * Store transactions
                          **/
-                        if ((string)o["action"] == "group" && (string)o["type"] == "synced") {
+                        if((string)o["action"] == "group" && (string)o["type"] == "synced") {
                             Helpers.SyncDelegate sd = (Helpers.SyncDelegate)CoreApplication.Properties["SyncDelegate"];
                             sd((JArray)o["data"]);
                             CoreApplication.Properties.Remove("SyncDelegate");
                         }
-                        if (o["conn_id"] != null) {
+                        if(o["conn_id"] != null) {
                             // authorize the connection using auth key recieved from http login
                             conn.SendMessage("{\"auth\": \"" + response.auth + "\"}");
                             CoreApplication.Properties["TCPSocket"] = conn;
@@ -222,7 +223,7 @@ namespace CritterCamp {
                         }
                     };
                     conn.pConnectionClosedEvent += TCPConnectionClosed;
-                    if (!conn.Connect()) {
+                    if(!conn.Connect()) {
                         System.Diagnostics.Debug.WriteLine("Error connecting to TCP server");
                         Status.Text = "Error connecting to server. Please try again.";
                     }
@@ -232,18 +233,22 @@ namespace CritterCamp {
                 // there was an error when connecting to the http server
                 System.Diagnostics.Debug.WriteLine("Error connecting to http server");
                 Status.Text = "Error connecting to server. Please try again.";
+                if(UserInput.Visibility == Visibility.Collapsed) {
+                    ResumeButton.Content = "Tap to Connect";
+                    ResumeButton.Visibility = Visibility.Visible;
+                }
             }
 
             connecting = false;
         }
-        
+
         void TCPConnectionClosed(bool error, TCPConnection connection) {
-            if (error) {
+            if(error) {
                 // connection timeout out
                 CoreApplication.Properties["error"] = "Lost connection to server.";
             }
             Dispatcher.BeginInvoke(() => {
-                if (NavigationService.CanGoBack) {
+                if(NavigationService.CanGoBack) {
                     NavigationService.GoBack();
                 }
             });
@@ -259,15 +264,16 @@ namespace CritterCamp {
             responseJSON = JObject.Parse(response);
             success = (string)responseJSON["status"] == "success" ? true : false;
             message = (string)responseJSON["message"];
-            if (message != null) 
+            if(message != null)
                 message = message.Trim();
         }
     }
 
-    class LoginResponse : Response{
+    class LoginResponse : Response {
         public string auth;
 
-        public LoginResponse(string response) : base (response) {
+        public LoginResponse(string response)
+            : base(response) {
             auth = (string)responseJSON["auth"];
         }
     }
